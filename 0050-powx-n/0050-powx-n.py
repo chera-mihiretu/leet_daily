e3b7df4d52bb2972967579efc7c62a2ff3dd1ada
Chera Mihiretu <63361469+chera-mihiretu@users.noreply.github.com>
2025-11-28 11:12:13 +0300

    Time: 0 ms (100%), Space: 17.6 MB (99.31%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -9,7 +9,23 @@ class Solution:
     def ppow(self, x, n):
         result = 1
         while (n > 0):
-            if n & 1: result *= x   
+            if (n & 1):result *= x
             x *= x
-            n //= 2
+            n >>= 1
         return result
+
+        """
+        
+        3 ^ (7) -> 3 ^ (4 + 2 + 1) 
+
+        3^4 * 3^2 * 3^1
+
+        7-> 111
+
+        2 
+        3 * 3 * 9 
+
+
+        
+        
+        """
